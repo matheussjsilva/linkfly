727676da6f17db3520efa937b35b727602990fbd
Matheus Silva <matheus.silva.contato26@gmail.com>
2025-01-10 21:44:28 -0300

    x

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.post("/shorten", (req, res) => {
   const { url } = req.body;
   const shortCode = nanoid(6);
   const shortUrl = `${(
-    process.env.BASE_URL || "https://linkfly.vercel.app/"
+    process.env.BASE_URL || "https://linkfly-production.up.railway.app"
   ).replace(/\/+$/, "")}/${shortCode}`;
 
   urlDatabase[shortCode] = url;
